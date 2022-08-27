7acf9d5aa2bc3c55b1ba81eebb68153528ed53ff
moronesia <moronesia8@gmail.com>
2022-08-28 01:00:10 +0700

    hook-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from "axios";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Axios from "axios";
+import "./App.css"
+
+
 
 function App() {
+  const [text, setText] = useState("Ketikan judul filmnya") 
+
+  const [movies, setMovies] =useState([])
+  
+
+  const changText = (event) => {
+    // console.log(event);
+    setText(event.target.value)
+  }
+
+  
+  const getMovies = (e) => {
+    e.preventDefault()
+
+    Axios.get(`http://www.omdbapi.com/?s=${text}&apikey=3c3331d6`)
+    .then((response)=>{
+      console.log(response);
+      setMovies(response.data.Search);
+    })
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Navbar bg="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Movies App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+            </Nav>
+            <Form className="d-flex" onSubmit={getMovies}>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                value={text}
+                onChange={changText}
+              />
+              <Button variant="outline-success" type="submit" >Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="container my-3">
+        <div className="row">
+          {
+            movies.map((value, Index) => {
+              return(
+                <div className="col-4" key={Index}>
+                  <div className="card" style={{width: "18rem"}}>
+                        <img src={value.Poster} className="card-img-top" alt="" />
+                        <div className="card-body">
+                          <h2 className="card-title">{value.Title}</h2>
+                          <h4 className="card-text">{value.Year}</h4>
+                      </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
+};
 
 export default App;
